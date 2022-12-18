269a194fe6fa1e16b55a05ed8c03398c1c3538d8
SebaRepko <sr305432@student.polsl.pl>
2022-12-18 23:30:27 +0100

    Dodanie metod

--- app/Http/Controllers/peoplecontroller.php
+++ app/Http/Controllers/peoplecontroller.php
@@ -8,6 +8,43 @@ class peoplecontroller extends Controller
 {
     public function index()
     {
-        echo "test";
+        $people = people::all();
+        return peopleresource::collection($people);
+    }
+
+    public function show(people $people)
+    {
+        //
+    }
+
+    public function edit(people $people)
+    {
+        //
+    }
+
+    public function update(peoplerequest $request, Post $people)
+    {
+        $people->update($request->all());
+        
+        return new peopleresource($people);
+    }
+
+    public function destroy(Post $people)
+    {
+        $post->delete();
+
+        return response(null, 204);
+    }
+
+    public function store(peoplerequest $request)
+    {
+        $people = people::create($request->all());
+        
+        return new peopleresource($people);
+    }
+
+    public function create()
+    {
+        //
     }
 }
