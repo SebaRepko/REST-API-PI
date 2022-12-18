7198ab4f884c6b156b12a9703ca64488591fc603
SebaRepko <sr305432@student.polsl.pl>
2022-12-18 21:07:11 +0100

    Create peoplecontroller
    Create peoplecontroller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\peoplecontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("Repko/305432",[peoplecontroller::class,'index']);
